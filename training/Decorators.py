848860179cc11b57718d46391f645391af59d99c
MathewAllen47 <allenmathew48@outlook.com>
2023-12-12 10:37:15 -0500

    fixed issues with return function

---
+++ training/Decorators.py
@@ -0,0 +1,33 @@
+#add functionality to a function/they surround/wrap the function with additional functionality
+
+def mydecorator(function):
+    def wrapper(*args,**kwargs):
+        #return_value= function(*args,**kwargs)
+        print("i am decorating your function",function.__name__)
+        return function(*args,**kwargs)#return_value
+    return wrapper
+
+
+def hello_world():
+    print("hello world")
+    
+
+mydecorator(hello_world)()
+
+@mydecorator #adds additional functionality
+def hello_world2():
+    print("hello world2")
+    #return "hello world2"
+
+hello_world2()
+#ISSUE, IT CANT TAKE INPUTS HERE /ABOVE 
+@mydecorator
+def hello(person):
+    #print(f"hello {person} person")
+    return f"hello {person}"
+
+print(hello("mike"))
+
+
+# hello("mike")
+
